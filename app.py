f9d91d7ca828c24d7c2f4f8b0a7a16d65a75d034
TelloCS <jeremytello31@gmail.com>
2024-11-04 16:10:50 -0600

    new changes

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, url_for, request, redirect
+from werkzeug.utils import secure_filename
+from werkzeug.exceptions import RequestEntityTooLarge
+import os
+from flask_sqlalchemy import SQLAlchemy
+from main import *
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["UPLOAD_DIRECTORY"] = 'uploads/'
+app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=["POST"])
+def upload():
+    try:
+        file = request.files['upload-code']
+        if file:
+            file.save(os.path.join(
+                app.config["UPLOAD_DIRECTORY"],
+                secure_filename(file.filename)
+            ))
+            files = os.listdir(app.config["UPLOAD_DIRECTORY"])
+            print(files)
+            return render_template('parse.html', files=files)
+    except RequestEntityTooLarge:
+        return "File is larger than the 16MB limit"
+    return redirect('/')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ main.py
@@ -0,0 +1,56 @@
+import ast
+from pprint import pp
+import pandas as pd
+
+# from graphviz import Digraph
+
+class Visitor(ast.NodeVisitor):
+    def __init__(self):
+        # self.tainted_vars = set()
+        # self.dot = Digraph()
+        self.data = []
+
+    def visit_Assign(self, node):
+        # targets -> [Attr, Name], value -> Name, Call, Sub
+        # print(node.__dict__)
+        # print((ast.unparse(node.targets), ast.unparse(node.value)))
+        # print(ast.unparse(node))
+        if (isinstance(node.value, ast.Call)
+            and isinstance(node.value.func, ast.Name)
+            and node.value.func.id == 'input'
+            ):
+            self.generic_visit(node)
+
+    def visit_Call(self, node):
+        # func -> name, attr; args -> [const, name, call]
+        # print(node.__dict__)
+        # print((ast.unparse(node.func), ast.unparse(node.args)))
+        # print(ast.unparse(node))
+        self.data.append({'nodeType': type(node).__name__,
+                          'tracked': ast.unparse(node),
+                          'line': node.lineno})
+        self.generic_visit(node)
+
+    def get_data(self):
+        return pd.DataFrame(self.data)
+    
+    # def visualize(self):
+    #     self.dot.render('output/graph', format='png', cleanup=True)
+
+def parse_file(code):
+    with open(code) as f:
+        code = f.read()
+
+    tree = ast.parse(code)
+    analyzer = Visitor()
+    analyzer.visit(tree)
+    return analyzer.get_data()
+
+# print(row)
+
+# if __name__ == "__main__":
+
+    # analyzer.visualize()
+
+    # print("\nProgram run:\n")
+    # exec(compile(code, "<ast>", mode="exec"))
